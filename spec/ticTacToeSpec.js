29d066d8bc5e16a9274d4da3e87375060f2da67b
kittysquee <kittysquee@gmail.com>
2017-05-18 16:39:26 +0100

    Adds js hint config files

--- spec/ticTacToeSpec.js
+++ spec/ticTacToeSpec.js
@@ -14,8 +14,18 @@ describe('TicTacToe', function(){
     expect(ticTacToe.currentPlayer).toEqual("X");
   });
 
-  it('will change the current player', function(){
-    ticTacToe.changePlayer()
+  it('will change the current player to 0', function(){
+    ticTacToe.changePlayer();
     expect(ticTacToe.currentPlayer).toEqual("0");
+  });
+
+  it('will change the current player back to X', function(){
+    ticTacToe.changePlayer();
+    ticTacToe.changePlayer();
+    expect(ticTacToe.currentPlayer).toEqual("X");
+  });
+
+  it('allows a play to occur', function(){
+    expect(ticTacToe.playTurn).to
   });
 });
